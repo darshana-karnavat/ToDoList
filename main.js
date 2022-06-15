1ddee1ba72d4963a0a9f324b44f305501ccecbc6
laird <laird@mail.com>
2022-06-15 17:52:48 +0200

    some desing

--- main.js
+++ main.js
@@ -1,47 +1,21 @@
+const aAddingTodos = () => {
+
+    const aNewTask = document.createElement('li');
+    aNewTask.setAttribute('class', 'list-group-item')
+    const aTaskValue = document.getElementById('a-my-input').value;
+    const aTaskText = document.createTextNode(aTaskValue);
+    const aErrorMessage = document.getElementById('error-message');
+    const aErrorMessageText = document.createTextNode('You must write some Tasks!');
+    const aClearError = () => aErrorMessage.innerText = '';
 
 
-const button = document.getElementById('add_button')
-const deleteEl = document.getElementById('delete');
-
-button.addEventListener('click', () => {
-
-    const task = document.querySelector('#fname').value;
-    const newTask = document.createElement('div#test');
-    newTask.innerHTML = `
-    <div id='test'>
-    <p>${task}</p>
-    <button id='delete' onclick='removeDiv()'>x</button>
-    </div>
-    `;
-    document.getElementById('result').appendChild(newTask);
-    
-}
-)
-
-const removeDiv = () => {
-    document.getElementById('test').remove()
-}
-
-
-
-// const addingTodos = () => {
-
-//     // const task = document.querySelector('#fname');
-//     const newTask = document.createElement('div');
-//     newTask.innerHTML = `<p>${task}</p>`
-// }
-
-
-// const body = document.querySelector('body');
-//DONT TOUCH!!!
-
-// document.body.innerHTML = '';
-// const myNewDiv = document.createElement('div');
-// myNewDiv.setAttribute('class', 'my-class');
-// myNewDiv.innerHTML = '<h1>Hello!</h1>';
-// document.body.appendChild(myNewDiv);
-
-
-// .setAttribute('class', 'my-class');
-// body.appendChild(myNewDiv);
-// .innerHTML = '<h1>Hello!</h1>';
+    aNewTask.appendChild(aTaskText);
+    if (aTaskValue === '') {
+        aClearError();
+        aErrorMessage.appendChild(aErrorMessageText)
+    } else {
+        document.getElementById('a-tasks').appendChild(aNewTask);
+        aClearError();
+    }
+    document.getElementById('a-my-input').value = '';
+    }
